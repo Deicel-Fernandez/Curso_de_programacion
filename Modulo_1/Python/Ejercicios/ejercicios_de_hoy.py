f30469413105cfc2ebc57bf23b1ad8bb712e6eeb
Deicel-Fernandez <fdeicela@gmail.com>
2025-06-27 12:31:07 -0400

    new

--- Modulo_1/Python/Ejercicios/ejercicios_de_hoy.py
+++ Modulo_1/Python/Ejercicios/ejercicios_de_hoy.py
@@ -1,7 +1,7 @@
 mi_diccionario = {
     "Deicel Fernandez": 30,
-    "Daniel Espitia": 25,
-    "Arianyelina Rincon": 15,
+    "Daniel Espitia": 18,
+    "Arianyelina Rincon": 21,
     "Brian Mendez": 10,
     "Carlos Villero": 18,
     "Damian Garcia": 13,
@@ -17,3 +17,29 @@ mi_diccionario = {
     "Yuliexy Dimas": 10
 }
 print(f"Diccionario original: {mi_diccionario}")
+
+mi_diccionario["Deicel Fernandez"] = 17
+mi_diccionario["Daniel Espitia"] = 25
+mi_diccionario["Arianyelina Rincon"] = 17
+mi_diccionario["Brian Mendez"] = 18
+mi_diccionario["Carlos Villero"] = 21
+mi_diccionario["Damian Garcia"] = 20
+mi_diccionario["Elias Rivas"] = 20
+mi_diccionario["Franklin Vecino"] = 20
+mi_diccionario["Helyanni Rodriguez"] = 21
+mi_diccionario["Liliana Rincon"] = 20
+mi_diccionario["Maria Macias"] = 20
+mi_diccionario["Over Machado"] = 19
+mi_diccionario["Paul Moran"] = 20
+mi_diccionario["Ronald Trujillo"] = 21
+mi_diccionario["Yuliexy Dimas"] = 20
+print(f"Diccionario original modificado: {mi_diccionario}")
+
+for clave in mi_diccionario:
+    print(f"Nombre: {clave}, Nota: {mi_diccionario[clave]}")
+
+for valor2 in mi_diccionario.values():
+    print("Nota:", valor2)
+########################################################################
+mi_conjunto = {1,2,3,4,5}
+print("Conjunto original:",mi_conjunto)
